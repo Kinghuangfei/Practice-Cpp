c7dd8388fb3ea821322415acbc4983571570ff38
coolsuperman <846069147@qq.com>
2019-05-06 16:11:51 +0800

    旋转等待多彩进度条

--- ColorProccesBar/main.cpp
+++ ColorProccesBar/main.cpp
@@ -5,16 +5,23 @@ using namespace std;
 
 class Solution{
   public: 
-    void ColorProccesBar(size_t size = 20){
-      for(size_t i = 0;i<size;++i){
+    void ColorProccesBar(size_t size = 100){
+      char round[4] ={'-','\\','|','/'}; 
+      for(size_t i = 0;i<=size;++i){
+          printf("%c[%%%d]",round[i%4],i);
         for(size_t j = 0;j<i;++j){
-          int a = (i%7)+30;
-          printf("\033[47;%dm#\033[0m",a);
+          if(i<=40)
+          printf("\033[47;31m#\033[0m");
+          else if(i>40&&i<=80)
+          printf("\033[47;33m#\033[0m");
+          else
+          printf("\033[47;32m#\033[0m");
         }
         fflush(stdout);
-        usleep(500000);
+        usleep(200000);
         printf("\r");
       }
+      printf("\n");
   }
 };
 
@@ -24,3 +31,4 @@ int main(){
   Solution().ColorProccesBar();
   return 0;
 }
+//////////////////////////////////////////////////
